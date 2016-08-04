a11144290836e07482ddab9c41baba2e4b3e8226
Muhammad Barmawi <muhammadbarmawi@outlook.com>
2016-08-04 11:49:44 +0300

    Add advanced class constructor snippet

---
+++ C#/advanced class constructor.cs
@@ -0,0 +1,33 @@
+﻿
+// the point() ,point(coords) call point(x,y)
+
+class point
+{
+  public int X { get; set; }
+  public int Y { get; set; }
+
+  public point() : this(0, 0)
+  {
+
+  }
+
+
+  public point(int coords) : this(coords, coords)
+  {
+
+  }
+
+  public point(int x, int y)
+  {
+    this.X = x;
+    this.Y = y;
+  }
+
+  public override string ToString()
+  {
+    return $"x : {X} , y: {Y}";
+  }
+}
+
+
+
