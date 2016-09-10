bec8509b05a47b42d39d30ff943d162f936eff3a
Muhammad Barmawi <muhammadbarmawi@outlook.com>
2016-09-10 13:52:37 +0300

    Add advanced project

---
+++ C#/Advanced/Delegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Delegate {
+  class Program {
+    delegate void PrintHello();
+    delegate int Square(int number);
+    static void Main(string[] args) {
+
+      // using lambda expressions
+      PrintHello writeHello = () => Console.WriteLine("Hello World");
+      writeHello(); // => Hello World
+      writeHello += () => Console.WriteLine("Hello from Another World");
+      writeHello.Invoke(); // => Hellow World , Hello from Another World
+
+    }
+
+
+  }
+}
