a9b8c65cae6f0077da30614a4207e111e17b6f72
mbarmawi <muhammadbarmawi@outlook.com>
2016-09-15 00:09:55 -0800

    Add async project

---
+++ C#/Advanced/Async/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(GetWebSiteHtml("http://google.com").GetAwaiter().GetResult());
+      Console.ReadKey();
+    }
+
+
+    static async Task<String> GetWebSiteHtml(string url) => await new WebClient().DownloadStringTaskAsync(url);
+  }
+}
