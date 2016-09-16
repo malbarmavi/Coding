912a8a97bec8dc0994da355a01468fa866633dd7
mbarmawi <muhammadbarmawi@outlook.com>
2016-09-15 16:37:42 -0800

    Add interfaces project

---
+++ C#/Advanced/Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using Interfaces.Person;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //Basic implementation
+
+      Student zain = new Student()
+      { Name = "Zain", LastName = "Albarmawi", Address = "TR,Istanbul", Id = "TR01", Age = 5 };
+      Employee omar = new Employee() { Name = "Omar", LastName = "Albarmawi", Address = "TR,Istanbul", Age = 23 };
+
+
+      Console.WriteLine("Printing : ");
+
+      PrintPersonInfo(zain);
+      PrintPersonInfo(omar);
+
+      Console.ReadKey();
+    }
+
+    static void PrintPersonInfo(IPerson person)
+    {
+      Console.WriteLine($"{person.FullName()}, Address: {person.Address}, IsMinor: {person.IsMinor()}");
+    }
+  }
+}
---
+++ C#/Advanced/Interfaces/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+  using Person;
+  public class Student : IPerson
+  {
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public float Age { get; set; }
+    public string Id { get; set; }
+    public string FullName() => $"{Id}, {Name} {LastName}, {Address}";
+
+    public bool IsMinor() => Age < 18;
+
+
+  }
+}
