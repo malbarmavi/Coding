d2ea396059272229b732245c6133d5cc8605978b
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2017-02-14 16:21:32 +0300

    Add typeof

---
+++ javascript/typeof .js
@@ -0,0 +1,32 @@
+// typeof operator examine a value and tell what type it is.
+
+var a ;
+console.log(typeof a);
+
+a = undefined;
+console.log(typeof a);
+
+a="hello world";
+console.log(typeof a);
+
+a=true;
+console.log(typeof a);
+
+a = 3;
+console.log(typeof a);
+
+a= 3.5;
+typeof a ;
+
+a = null;
+console.log(typeof a);
+
+a= {};
+console.log(typeof a);
+
+a = function() {}
+console.log(typeof a);
+
+a= [];
+console.log(typeof a);
+
